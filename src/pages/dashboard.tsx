63ae2e84d765eb92b140f481fa29bae67e2e8f0f
Tijs Martens <tijs@bothrs.com>
2022-06-04 02:51:15 +0200

    feat: get user ids from airtable

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -19,19 +19,38 @@ export async function getServerSideProps(context: NextPageContext) {
         method: 'POST',
       }
     )
-    const tokenData = await tokenResponse.json()
-    //GET ALL USERS FROM CLUB
-    const clubResponse = await fetch(
-      `https://www.strava.com/api/v3/clubs/${process.env.STRAVA_CLUB_ID}/members`,
+    // const tokenData = await tokenResponse.json()
+    // //GET ALL USERS FROM CLUB
+    // const clubResponse = await fetch(
+    //   `https://www.strava.com/api/v3/clubs/${process.env.STRAVA_CLUB_ID}/members
+    //   `,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${tokenData.access_token}`,
+    //     },
+    //   }
+    // )
+    // const clubData = await clubResponse.json()
+    // //GET ALL DATA FROM USERS IN CLUB
+
+    // console.log(clubData)
+    // const userIds = clubData
+
+    console.log('token airtable', process.env.AIRTABLE_API_KEY)
+
+    const usersPromise = await fetch(
+      'https://api.airtable.com/v0/appVzgNst5ii1659k/participants?maxRecords=3&view=Grid%20view',
       {
         headers: {
-          Authorization: `Bearer ${tokenData.access_token}`,
+          Authorization: ('Bearer ' + process.env.AIRTABLE_API_KEY) as string,
         },
       }
     )
-    const clubData = await clubResponse.json()
-    //GET ALL DATA FROM USERS IN CLUB
 
+    const airtableUser = await usersPromise.json()
+    const userIds = airtableUser?.records
+      .map((user: unknown) => user.fields.profile_url)
+      .map((url: string) => url.split('/').at(-1))
     return {
       props: {}, // will be passed to the page component as props
     }
